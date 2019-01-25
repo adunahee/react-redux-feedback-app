4d3d2308d0f6573b64fb90fc94cd9199b2adfb61
Anthony D <dunahee@gmail.com>
2019-01-25 17:26:45 -0600

    have a server router and working get request for all feedback on db

---
+++ server/routers/feedback.router.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const pg = require('pg');
+
+const pool = new pg.Pool({
+    host: 'localhost',
+    port: 5432, 
+    database: 'prime_feedback',
+    max: 10,
+    idleTimeoutMillis: 10000,
+});
+
+router.get('/', (req, res) => {
+    pool.query('SELECT * FROM "feedback";').then(result => {
+        res.send(result.rows);
+    }).catch(error => {
+        console.log(`Error in feedback router get`, error);
+        res.sendStatus(500);
+    });
+});
+
+module.exports = router;
