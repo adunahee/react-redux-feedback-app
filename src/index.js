01ba2c2582e9b78e42ad8efe563d52b91c23c3e3
Anthony D <dunahee@gmail.com>
2019-01-25 17:11:41 -0600

    setup reduxStore with default state

--- src/index.js
+++ src/index.js
@@ -4,5 +4,24 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//imports for using redux store and logger dev tool
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import logger from 'redux-logger';
+
+//reducer to store feedback in state as object with key, properties
+const feedbackDefaultState = {feeling: 3, understanding: 4, support: 5, comments: "Pulling through!"}
+const feedback = (state = feedbackDefaultState, action) => {
+    //action types for each page of the feedback
+    return state;
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        feedback,
+    }),
+    applyMiddleware(logger),
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
